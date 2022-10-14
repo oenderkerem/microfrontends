ada1e3052f207fe61576a65d682988fe8b61dca7
oenderkerem <oender.yaz@outlook.com>
2022-10-14 14:29:14 +0200

    intial commit

---
+++ src/main/java/de/oenderyaz/microfrontends/MicrofrontendsApplication.java
@@ -0,0 +1,13 @@
+package de.oenderyaz.microfrontends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrofrontendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrofrontendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/oenderyaz/microfrontends/MicrofrontendsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.oenderyaz.microfrontends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrofrontendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
